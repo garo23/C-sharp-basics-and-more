b3b76f5f90d30d6e6ff4e29628438fd72da12e6d
garo23 <107251338+garo23@users.noreply.github.com>
2022-06-13 15:26:09 +0300

    Update diffBtwDays.cs

--- diffBtwDays.cs
+++ diffBtwDays.cs
@@ -2,7 +2,7 @@
 
 public class Class1
 {
-	public Class1(int year,int month, int day,int year2, int month2, int day2)
+	public static long Class1(int year,int month, int day,int year2, int month2, int day2)
 	{
 		DateTime start = new DateTime(year, month, day);
 		DateTime end = new DateTime(year2, month2, day2);
