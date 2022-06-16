f1c1ec5827985cfead1e810fc258ef4bae8829ae
garo23 <107251338+garo23@users.noreply.github.com>
2022-06-16 11:30:09 +0300

    Add files via upload

---
+++ CreditCardMask.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+//"4556364607935616" --> "############5616"
+//"64607935616"-- > "#######5616"
+//"1"-- > "1"
+//""-- > ""
+public class Maskify
+{
+    public static String maskify(String str)
+    {
+        char[] strChars = str.toCharArray();
+        for (int i = 0; i < strChars.length - 4; i++)
+        {
+            strChars[i] = '#';
+        }
+        return new String(strChars);
+    }
+}
