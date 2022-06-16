00295b560a30c38a520ec50220eef70b44046bf8
garo23 <107251338+garo23@users.noreply.github.com>
2022-06-16 10:53:38 +0300

    Add files via upload

---
+++ HumanReadableTime.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+public static class TimeFormat
+{
+    public static string GetReadableTime(int seconds)
+    {
+        int hours = seconds / 3600;
+        int minutes = (seconds / 60) % 60;
+        int s = seconds % 60;
+
+        return (hours.ToString("00") + ":" + minutes.ToString("00") + ":" + s.ToString("00"));
+    }
+}
