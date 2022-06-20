4984a5c68cc0ecc5929ab5f7bacffc0cf0ba6a78
garo23 <107251338+garo23@users.noreply.github.com>
2022-06-20 13:32:58 +0300

    Exercisesarrays

---
+++ Array1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+
+class ArrayCount
+{
+    public void ArrCount()
+    {
+        int[]arr = new int[20];
+        for(int i = 0; i < 20; i++)
+        {
+            arr[i] = i * 5;
+
+        }
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.WriteLine(arr[i]);
+        }
+    }
+}
---
+++ Array6.cs
@@ -0,0 +1,32 @@
+﻿class subseq
+{
+    public  int[]  findLongestConseqSubseq(int a, int[] arr)
+    {
+        Array.Sort(arr);
+        int count = 0; //counts the length of the subsequence
+        
+        List<int> list = new List<int>();
+        for (int i = 0; i < a; i++)
+        {
+            if (arr[i] != arr[i - 1])
+            {
+
+                list.Add(arr[i]);
+            }
+        }
+        for(int i = 0; i<list.Count; i++)
+        {
+            if (arr[i] == arr[i - 1] + 1)
+            {
+                list.Add(arr[i]);
+                count++;
+            }
+            else
+                count = 1;
+        }
+        return list.ToArray();
+
+
+    }
+}
+
