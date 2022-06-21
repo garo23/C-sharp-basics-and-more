ac5bd962aaad2193116dbb95a4c2ace6bb992725
garo23 <107251338+garo23@users.noreply.github.com>
2022-06-21 12:23:44 +0300

    addthenewtests

---
+++ Arrays10.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class mf{
+    public static int mostfrequent(int n)
+    {
+        int[] arr = new int[n];
+        Array.Sort(arr);
+        int count = 0;
+        int max_count = 1;
+           int res = arr[0];
+
+        for (int i =0; i<n; i++)
+        {
+            if (arr[i] == arr[i - 1])
+            {
+                count++;
+            }
+            else count = 1;
+
+            if (count > max_count)
+            {
+                max_count = count;
+                res = arr[i - 1];
+            }
+        }
+        return res;
+    }
+
+
+}
--- Program.cs
+++ Program.cs
@@ -3,7 +3,13 @@
 class MainFunc{
     static void Main()
     {
-        
+        int a = int.Parse(Console.ReadLine());
+        maxsubseq maxsubseq = new maxsubseq();
+
+        Console.WriteLine(maxsubseq.maxsum(a));
+
+
+
     }
 }
 
