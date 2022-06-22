1314fb3c802134287fa12966a06697573388c22b
garo23 <107251338+garo23@users.noreply.github.com>
2022-06-22 11:17:05 +0300

    Add files via upload

---
+++ CSharp-ArrayExercises/Arrays11.cs
@@ -0,0 +1,35 @@
+﻿// Write a program to find a sequence of neighbor numbers in an array, which has a sum of certain number S.
+// Example: {4, 3, 1, 4, 2, 5, 8}, S=11 -> {4, 2, 5}.
+class Arrays11
+{
+    public void Ex9()
+    {
+        int sum = 0, tempSum;
+
+        Console.Write("Enter array length: ");
+        int length = Int32.Parse(Console.ReadLine());
+
+        int[] arr = new int[length];
+
+        for (int i = 0; i < length; i++)
+        {
+            Console.Write("Enter {0} element: ", i);
+            arr[i] = Int32.Parse(Console.ReadLine());
+        }
+
+        for (int i = 0; i < length - 1; i++)
+        {
+            tempSum = arr[i];
+
+            for (int j = i + 1; j < length; j++)
+            {
+                tempSum += arr[j];
+                if (tempSum > sum) sum = tempSum;
+            }
+        }
+
+        Console.WriteLine("Result is {0}. ", sum);
+
+
+    }
+}
---
+++ CSharp-ArrayExercises/Arrays15.cs
@@ -0,0 +1,22 @@
+﻿//Write a program, which creates an array containing all Latin letters.
+//The user inputs a word from the console
+//and as result the program prints to the console the indices of the letters from the word.
+//
+class word
+{
+    public void indexofletter()
+    {
+        char[] alphabet = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+        Console.Write("Enter a word: ");
+        string sentence = (Console.ReadLine());
+        char[] word = sentence.ToCharArray();
+
+        for (int i = 0; i < word.Length; i++)
+            for (int j = 0; j < alphabet.Length; j++)
+                if (word[i] == alphabet[j]) Console.Write("{0} ", j);
+
+    } 
+
+
+}
