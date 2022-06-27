118f7001e47f4ff82dfe8d7a7d8e7d4e30cf6a3c
garo23 <107251338+garo23@users.noreply.github.com>
2022-06-27 14:43:30 +0300

    Add files via upload

---
+++ Exceptions/Sqrt.cs
@@ -0,0 +1,12 @@
+﻿public class Sqrt
+{
+    public static double SQRT(double value)
+    {
+        if (value < 0)
+        {
+            throw new System.ArgumentOutOfRangeException("value","Sqrt for negative numbers is undefined");
+        }
+        return Math.Sqrt(value);
+    }
+
+}
