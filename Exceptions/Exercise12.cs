c49440981a844de448a639bc6b674af485c338a8
garo23 <107251338+garo23@users.noreply.github.com>
2022-06-27 15:55:05 +0300

    Add files via upload

---
+++ Exceptions/Exercise12.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_12_Solution_6
+{
+    class Exercise12
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(@"C:\Users\gaprahamyan\Desktop\Arrays\Arrays\Arrayss"))
+                {
+                    String line = sr.ReadToEnd();
+                    Console.WriteLine(line);
+                }
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (DirectoryNotFoundException e)
+            {
+                Console.WriteLine("The specified path is invalid!");
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Invalid syntax!");
+            }
+        }
+    }
+}
