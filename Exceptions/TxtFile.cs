b3eab92ae2d94fa5dcab121253fbccd9ddd722f0
garo23 <107251338+garo23@users.noreply.github.com>
2022-06-27 15:23:02 +0300

    Add files via upload

---
+++ Exceptions/TxtFile.cs
@@ -0,0 +1,40 @@
+﻿public class TxtFile
+{
+    public void ReadFile(String filename)
+    {
+        StreamReader reader = null;
+        try
+        {
+            reader = new StreamReader(filename);
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                Console.WriteLine(line);
+
+            }
+            reader.Close();
+
+        } catch (FileNotFoundException)
+        
+        {
+               Console.WriteLine("The file '{0}' does not exist.", filename);
+
+
+        }
+        catch (UnauthorizedAccessException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        finally
+        {
+            if(reader != null)
+            {
+
+                reader.Close();
+            }
+
+        }
+    
+    }
+
+}
