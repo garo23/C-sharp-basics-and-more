eba952295cfab6874e3cdb2d54d2e6c5a4810266
garo23 <107251338+garo23@users.noreply.github.com>
2022-06-27 14:37:14 +0300

    Add files via upload

---
+++ Exceptions/InvalidNumberException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class InvalidNumberException : Exception {
+    private int _startValue;
+    private int _endValue;
+    public InvalidNumberException(int startValue, int endValue)
+    {
+        _startValue = startValue;
+        _endValue = endValue;
+    }
+
+    public override string Message => $"Invalid number. It should be between {_startValue} and {_endValue}";
+}
+
+
---
+++ Exceptions/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.Write("Enter start value:");
+int start = int.Parse(Console.ReadLine());
+Console.Write("Enter end value:");
+int end = int.Parse(Console.ReadLine());
+
+for(int i =0; i<10; i++)
+{
+    try
+    {
+        ReadNumber(start, end);
+    }catch(InvalidNumberException e)
+    {
+        Console.WriteLine(e.Message);
+    }catch(FormatException e)
+    {
+        Console.WriteLine(e.Message);
+    }
+    finally
+    {
+        Console.WriteLine("Good bye");
+    }
+}
+
+
+ void ReadNumber( int start, int end)
+{
+   
+    int num = int.Parse(Console.ReadLine());
+    if (num < start || num > end)
+    {
+        throw new InvalidNumberException(start, end);
+    }
+
+}
+
+
