04663f563401a9e5e6109598d0eabc0102ee577a
garo23 <107251338+garo23@users.noreply.github.com>
2022-06-28 13:18:35 +0300

    Add files via upload

---
+++ Strings/Paranthesis.cs
@@ -0,0 +1,27 @@
+﻿public class Paranthesis
+{
+    public static bool validParanthesis(String input)
+    {
+        int countleft = 0;
+        int countright = 0;
+        char []arr = new char [input.Length];
+       arr  = input.ToCharArray();
+        for(int i = 0; i < arr.Length; i++)
+        {
+            if(arr[i] == '(')
+            {
+                countleft++;
+            }else if(arr[i] == ')')
+            {
+                countright++;
+            }
+            if (countleft==countright)
+            {
+                return true;
+            }
+        }
+        return false;
+
+    }
+
+}
---
+++ Strings/StringReverse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text;
+public class StringReverse
+{
+
+    public static string ReverseString(string str)
+    {
+        StringBuilder sb = new StringBuilder();
+        for(int i = str.Length; i>=0; i--)
+        {
+            sb.Append(str[i]);
+        }
+        return sb.ToString();
+    }
+}
