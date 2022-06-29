66df4f57f8182d3182ca175d2bd7a9351b118e72
garo23 <107251338+garo23@users.noreply.github.com>
2022-06-29 12:46:39 +0300

    Add files via upload

---
+++ Strings/Occurences.cs
@@ -0,0 +1,30 @@
+﻿public class Occurences
+{
+    public static int counter(string substring,string text)
+    { 
+        string word = string.Empty;
+        int substringcounter = 0;
+
+        for(int i = 0; i < text.Length; i++)
+        {
+            word += text[i];
+            if(word.Length == substring.Length)
+            {
+                if(word.ToLower() == substring.ToLower())
+                {
+                    substringcounter++;
+                }
+                word = String.Empty;
+                word += text[i];
+
+            }
+        }  
+        return substringcounter;    
+
+    }
+    
+    
+    
+
+    
+}
