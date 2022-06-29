431cd1a47ca23b448191af232fe54000353b466d
garo23 <107251338+garo23@users.noreply.github.com>
2022-06-29 13:42:21 +0300

    Add files via upload

---
+++ Strings/Excercise7.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+class Excercise7
+{
+    public static void symbols(string input)
+    {
+        string line;
+        line = Console.ReadLine();
+        if (line.Length > 20)
+        {
+            for(int i =0; i<20; i++)
+            {
+                Console.WriteLine(line[i]);
+            }
+            Console.WriteLine();
+
+        }
+        else
+        {
+            string newLine = line.PadRight(20, '*'); //имаме общо 20 символа 20=символите от line + толкова '*' до 20 
+            Console.WriteLine(newLine); 
+
+        }
+
+    }
+}
