f50ca54f94b84b2332805d6ab111e516b1d9e317
garo23 <107251338+garo23@users.noreply.github.com>
2022-06-30 11:50:12 +0300

    Add files via upload

---
+++ Strings/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more informationint
+string text = Console.ReadLine();
+string[] forbidenWords = Console.ReadLine().Split(','); //array of words
+
+for (int i = 0; i < forbidenWords.Length; i++)
+{
+    int existForbidenWord = 0;
+    if ((existForbidenWord = text.IndexOf(forbidenWords[i])) >= 0) //checks if forbiden word exists
+    {
+        string replaced = new string('*', forbidenWords[i].Length);
+        //replaces the length of the word with'*' same length
+        text = text.Replace(forbidenWords[i], replaced);
+
+    }
+
+}
+Console.WriteLine(text);
+
+
