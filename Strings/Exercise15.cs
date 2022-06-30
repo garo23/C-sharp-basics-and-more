8956a26335dcb90404acdce43604317e94bf1120
garo23 <107251338+garo23@users.noreply.github.com>
2022-06-30 14:34:42 +0300

    Add files via upload

---
+++ Strings/Exercise15.cs
@@ -0,0 +1,17 @@
+﻿class Exercise15
+{
+    public static void dictionary(string input)  {
+         Dictionary<string, string> dict = new Dictionary<string, string>();
+
+        dict.Add(".NET", "platform for applications from Microsoft");
+        dict.Add("CLR", "managed execution enviroment for .NET");
+        dict.Add("namespace", "hiererchical organisation of classes");
+
+        Console.WriteLine(dict[input]);
+
+      }
+
+
+
+}
+        
