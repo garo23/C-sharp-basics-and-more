23b88a74cc7e12530cccabc4802b0a3cb32fcf2b
garo23 <107251338+garo23@users.noreply.github.com>
2022-06-30 15:14:28 +0300

    Add files via upload

---
+++ Strings/Exercise16.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text.RegularExpressions;
+class Exercise16
+{
+    public static void hyperlinks(string text)
+    {
+        string newtext = Regex.Replace(text, @"<a href=""", "[URL="); //(къде заместваме, кое заместваме, с какво го заместваме)
+        newtext = Regex.Replace(newtext, @""">", "] ");
+        newtext = Regex.Replace(newtext, "</a>", "[/URL]");
+
+        Console.WriteLine();
+        Console.WriteLine(newtext);
+
+
+    }
+
+}
