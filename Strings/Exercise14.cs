adef5d92cf29bdb1a63c34b94555a22de07476b0
garo23 <107251338+garo23@users.noreply.github.com>
2022-06-30 14:01:41 +0300

    Add files via upload

---
+++ Strings/Exercise14.cs
@@ -0,0 +1,26 @@
+﻿// http://www.devbg.org/forum/index.php резултатът е:
+//[protocol]= "http"
+//[server] = "www.devbg.org"
+//[resource] = "/forum/index.php"
+
+using System.Text;
+class Exercise14
+{
+    public static void stringreverse(string input)
+    {
+        string[] arr = input.Split(" ");
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = arr.Length-1; i>=0; i--)
+        {
+            sb.Append(arr[i]);
+
+
+        }
+        Console.WriteLine(sb.ToString());
+
+    }
+
+
+
+}
