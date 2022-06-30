19df1aa832f0df419fb154f18cc6b761ff78e08e
garo23 <107251338+garo23@users.noreply.github.com>
2022-06-30 17:38:00 +0300

    Add files via upload

---
+++ Strings/Exercise19.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+class Exercise19
+{
+    public static bool TestEmailRegex(string emailAdress)
+    {
+        string patternStrict = "^([a - zA - Z0 - 9_.] +)@([a - zA - Z0 - 9_] +)([a-zA-Z]{2,5})$";
+
+        Regex reStrict = new Regex(patternStrict);
+        bool isStrictMatch = reStrict.IsMatch(emailAdress);
+
+        return isStrictMatch;
+
+    }
+}
+
---
+++ Strings/Palindrome21.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class Palindrome21
+{
+     public static bool IsPalindrome(string word)
+    {
+        string[]arr = word.Split(' ');  
+        for(int i = 0; i < word.Length/2; i++)
+        {
+            if (arr[i].ToLower() == arr[arr.Length - 1 - i].ToLower())
+            {
+                continue;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+}
