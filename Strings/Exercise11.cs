5d9e2aeea4b7a7e60b56d937deb581b9b764cff8
garo23 <107251338+garo23@users.noreply.github.com>
2022-06-30 11:42:19 +0300

    Add files via upload

---
+++ Strings/Exercise11.cs
@@ -0,0 +1,23 @@
+﻿//
+
+public class Exercise11
+{
+    public static string ForbiddenWords(string text)
+    {
+        string[] forbidenWords = Console.ReadLine().Split(','); //array of words
+        
+        for(int i = 0; i < forbidenWords.Length; i++)
+        {
+            int existForbidenWord = 0;
+            if ((existForbidenWord = text.IndexOf(forbidenWords[i])) >= 0) //checks if forbiden word exists
+            {
+                string replaced = new string('*', forbidenWords[i].Length); 
+                //replaces the length of the word with'*' same length
+                text = text.Replace(forbidenWords[i], replaced);
+
+            }
+
+        }
+        return text;
+    }
+}
