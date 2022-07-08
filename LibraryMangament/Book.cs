4189eb382fe03023ea133143f568584161060eab
garo23 <107251338+garo23@users.noreply.github.com>
2022-07-08 10:11:03 +0300

    Add files via upload

---
+++ LibraryMangament/Book.cs
@@ -0,0 +1,24 @@
+﻿class Book
+{
+    private string _Title;
+    private string _Publishers;    
+    private string _Author;
+    private int _Year;
+    private string _ISBN;
+
+    public Book(string title, string publishers, string author, int year, string iSBN)
+    {
+        _Title = title;
+        _Publishers = publishers;
+        _Author = author;
+        _Year = year;
+        _ISBN = iSBN;
+    }
+    public string Title { get { return _Title; } }
+    public string Publishers { get { return _Publishers; } }
+    public string Author { get { return _Author; } }  
+    public int Year { get { return _Year; } } 
+    
+    public string ISBN { get { return _ISBN; } }    
+        
+}
---
+++ LibraryMangament/Library.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+class Library {
+    private List <Book> _BooksInLibrary;
+    private string _Name;
+
+    public  List<Book> addBook(Book b)
+    {
+        _BooksInLibrary.Add(b);
+        return _BooksInLibrary;
+    }
+
+    public List<Book> removeBook(String author)
+    {
+        foreach(Book b in _BooksInLibrary)
+        {
+            if(b.Author == author)
+            {
+                _BooksInLibrary.Remove(b);
+            }
+        }
+        return _BooksInLibrary;
+    }
+
+}
+
+
+
