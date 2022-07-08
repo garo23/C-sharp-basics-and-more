9b3f3132df99e3d6369b4322124c21bd8e5238eb
garo23 <107251338+garo23@users.noreply.github.com>
2022-07-08 11:12:03 +0300

    Add files via upload

---
+++ GSM/PhoneTest.cs
@@ -0,0 +1,16 @@
+﻿class PhoneTest
+{
+    public static void PhoneFactory()
+    {
+        Phone[] phones = new Phone[3];
+        phones[0] = new Phone("Redmi9", "Xiaomi", "200$");
+        phones[1] = new Phone("Iphone13", "Apple", "800$");
+        phones[2] = new Phone("A52", "Samsung", "300$");
+
+        for(int i = 0; i < phones.Length; i++)
+        {
+            phones[i].GetInfo();
+            Console.WriteLine(" ");
+        }
+ }
+}
---
+++ GSM/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Phone Nokia95 = new Phone("95", "Nokia", "25$");
+Nokia95.GetInfo();
