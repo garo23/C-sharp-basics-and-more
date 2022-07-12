c09f01b7861502450d848dbe1277dad8c71e304f
garo23 <107251338+garo23@users.noreply.github.com>
2022-07-12 14:06:23 +0300

    Add files via upload

---
+++ TextFiles/Exercise3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+class Exercise3
+{
+    public static void LineNumber()
+    {
+
+        try
+        {
+            StreamReader reader = new StreamReader("text2.txt");
+            using (reader)
+            {
+                int lineNumber = 0;
+                string line = reader.ReadLine();
+                while (line != null)
+                {
+                    lineNumber++;
+                    Console.WriteLine("Line {0}: {1}", lineNumber, line);
+                    line = reader.ReadLine();
+                }
+            }
+        }
+        catch (FileNotFoundException e)
+        {
+            Console.WriteLine("An error has occured" + e.Message);
+        }
+
+    }
+
+}
---
+++ TextFiles/Exercise7.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+class Exercise7
+{
+    public static void SF()
+    {
+        var reader = new StreamReader(@"../startfinish.txt", Encoding.UTF8);
+        using (reader)
+        {
+            var line = reader.ReadLine();
+            line.Replace("start", "finish");
+            Console.WriteLine(line);    
+            while (!string.IsNullOrWhiteSpace(line)) { 
+            line = reader.ReadLine();
+            line.Replace("start", "finish");
+                Console.WriteLine(line);
+            }
+
+        }
+
+    }
+}
