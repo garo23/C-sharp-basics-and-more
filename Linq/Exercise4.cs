1ec0f48597d727d0653de9b9d7b72cc078928aa1
garo23 <107251338+garo23@users.noreply.github.com>
2022-07-27 12:34:20 +0300

    Update Exercise4.cs

--- Linq/Exercise4.cs
+++ Linq/Exercise4.cs
@@ -8,5 +8,23 @@ namespace LINQ
 {
     internal class Exercise4
     {
+        //Student[] arr = {
+    //new Student() {firstName = "Ivan", lastName = "Ivanov", age = 21 },
+    //new Student() { firstName = "Georgi", lastName = "Georgiev", age = 25 },
+    //new Student() { firstName = "Mario", lastName = "Metushev", age = 24 },
+//}; 
+        
+        var youngStud =
+            from stud in arr
+            where stud.age >= 21 && stud.age <= 24
+            select stud;
+        Console.WriteLine("Students between 21 and 24 years");
+        
+           foreach (var stud in youngStud)
+{  
+    Console.WriteLine(stud.firstName + stud.lastName + stud.age);
     }
-}
+        
+  }
+}    
+
