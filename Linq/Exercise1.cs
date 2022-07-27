e4bd17b34be923a6b6c9a980fc0cf290d0fed1d0
garo23 <107251338+garo23@users.noreply.github.com>
2022-07-27 12:35:25 +0300

    Update Exercise1.cs

--- Linq/Exercise1.cs
+++ Linq/Exercise1.cs
@@ -1,4 +1,16 @@
 ﻿using System.Text;
+
+public static void Main(){
+
+string input = "Some Text To Try The Substring Extension Method";
+int startIndex = int.Parse(Console.ReadLine());
+int finalIndex = int.Parse(Console.ReadLine());
+
+StringBuilder finalinput = new StringBuilder(input);
+Console.WriteLine(finalinput.JustTheMethod(startIndex,finalIndex)); 
+
+}
+
 public static partial class Exercise1 {
     
     public static string JustTheMethod( this StringBuilder sb , int startindex, int finalindex) {
