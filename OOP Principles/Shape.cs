cbfc04ae17d987ee18d309959794a80b1d2ea72f
garo23 <107251338+garo23@users.noreply.github.com>
2022-08-08 11:14:22 +0300

    Add files via upload

---
+++ OOP Principles/Shape.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomeShapes
+{
+    public abstract class Shape
+    {
+        public static double length;
+        public static double width;
+
+        public virtual double Width
+        {
+            get { return width; }
+            set
+            {
+                if (value < 0)
+                {
+                   throw new ArgumentOutOfRangeException("Please enter correct width");
+                }
+                width = value;
+            }
+        }
+
+        public virtual double Length
+        {
+            get { return length; }
+            set
+            {
+
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Please enter correct width");
+                }
+                { length = value; }
+            }
+        }
+       
+        
+        public abstract double CalculateSurface();
+        
+    }
+}
---
+++ OOP Principles/Triangle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomeShapes
+{
+    public class Triangle : Shape {
+
+        public Triangle() { }   
+        public override double CalculateSurface()
+        {
+            return Length * Width/2;
+            
+        }
+    }
+}
